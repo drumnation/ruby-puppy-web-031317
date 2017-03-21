05992b7e6d5d44f9f26f4d2615e71d66b018241e
David Mieloch <davidmieloch@gmail.com>
2017-03-21 02:10:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all.push(self)
+  end
+
+  def self.all
+    puts @@all.collect { |dog| dog.name }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
